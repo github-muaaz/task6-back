7b444b52be08942a8c66bc7dd32a04e566051cb8
Muaaz [Muhammadaziz] <muhammadaziz.erg.1229@gmail.com>
2024-05-04 19:16:03 +0500

    api changed

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const { Server } = require('socket.io');
 const io = new Server({
-    cors: "https://itransition-task6-muaaz.netlify.app"
-})
-io.on('connection', function (socket) {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
+io.on('connection', function (socket) {
     socket.on('board', (data) => {
         socket.broadcast.emit('board', data);
     });
